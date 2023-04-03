bc1117b9731fe941b399562b2f7a50dc8f852cfb
Andres Abarca Valenzuela <avalenzuela.andres@gmail.com>
2023-04-03 00:09:36 -0400

    Transferencia de aprendizaje

---
+++ main.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+
+
+# Aumento de datos con ImageDataGenerator
+# Crear el generador
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=30,
+    width_shift_range=0.25,
+    height_shift_range=0.25,
+    shear_range=15,
+    zoom_range=[0.5, 1.5],
+    validation_split=0.2  # 20% para pruebas
+)
+
+# Generadores para sets de entrenamiento y pruebas
+data_gen_entrenamiento = datagen.flow_from_directory('datasets', target_size=(224, 224), batch_size=32, shuffle=True,
+                                                     subset='training')
+data_gen_pruebas = datagen.flow_from_directory('datasets', target_size=(224, 224), batch_size=32, shuffle=True,
+                                               subset='validation')
+
+# Imprimir 10 imagenes de entrenamiento
+for imagen, etiqueta in data_gen_entrenamiento:
+    for i in range(10):
+        plt.subplot(2, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(imagen[i])
+    break
+
+plt.show()
+
+url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
+
+# Crear modelo
+modelo = tf.keras.Sequential([
+    hub.KerasLayer(url, input_shape=(224, 224, 3), trainable=False),
+    tf.keras.layers.Dense(3, activation='softmax')
+])
+
+modelo.summary()
+
+# Compilar modelo
+modelo.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entrenar modelo
+epochs = 50
+historial = modelo.fit(data_gen_entrenamiento, epochs=epochs, batch_size=32, validation_data=data_gen_pruebas)
+
+# Exportar modelo
+modelo.save('modelo_transfer_learning.h5')
---
+++ test.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from io import BytesIO
+import requests
+from PIL import Image
+import cv2
+import numpy as np
+import tensorflow_hub as hub
+
+# Cargar modelo
+modelo = tf.keras.models.load_model('modelo_transfer_learning.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+
+
+def categorizar(url):
+    respuesta = requests.get(url)
+    img = Image.open(BytesIO(respuesta.content))
+    img = np.array(img).astype(float) / 255
+
+    img = cv2.resize(img, (224, 224))
+    prediccion = modelo.predict(img.reshape(-1, 224, 224, 3))
+    return np.argmax(prediccion[0], axis=-1)
+
+
+# Debe ser 1
+predict_teclado = categorizar('https://androidpc.es/wp-content/uploads/2021/03/teclado-ingles-internacional-n00.jpg')
+print(predict_teclado)
+
+# Debe ser 2
+predict_mouse = categorizar('https://d1pc5hp1w29h96.cloudfront.net/magefan_blog/mouse_ptico_vs_l_ser.jpg')
+print(predict_mouse)
+
+# Debe ser 0
+predict_headphones = categorizar('https://helios-i.mashable.com/imagery/reviews/01HUTTQhSs8SWLx3ouc0f7q/hero-image.fill.size_1200x1200.v1653070267.jpg')
+print(predict_headphones)
